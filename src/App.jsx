bae0c5b5b423a75c3b86440e2acd6d8303f59832
JazminCaruso <carusojazmin@gmail.com>
2023-11-24 20:27:36 -0300

    tablet vista

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import Inicio from './screens/Inicio'
 import QuienesSomosScreen from './screens/QuienesSomosScreen'
 import ContactoScreen from './screens/ContactoScreen'
 import ServiciosScreen from './screens/ServiciosScreen'
-import NosotrasScreen from './screens/NosotrasScreen'
-import EquipoScreen from './screens/EquipoScreen';
-import MisionVisionScreen from './screens/MisionVisionScreen';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -21,10 +18,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Inicio/>}/>
           <Route path="/quienes-somos" element={<QuienesSomosScreen/>}/>
-          <Route path="/contacto" element={<ContactoScreen/>}/>
-          <Route path="/quienes-somos/sobre-nosotras" element={<NosotrasScreen/>}/>
-          <Route path="/quienes-somos/equipo" element={<EquipoScreen/>}/>
-          <Route path="/quienes-somos/mision-vision" element={<MisionVisionScreen/>}/>
           <Route path="/servicios" element={<ServiciosScreen/>}/>
           <Route path="/contacto" element={<ContactoScreen/>}/>
         </Routes>
